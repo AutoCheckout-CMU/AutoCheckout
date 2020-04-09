3f09aa266b1b52a67c80cb51bd5a703d65efa840
Mengmeng ZHANG <qiuyu8290@gmail.com>
2020-04-08 19:30:39 -0700

    ground truth for test-cps-01

--- GroundTruthHelper.py
+++ GroundTruthHelper.py
@@ -1,8 +1,9 @@
 import BookKeeper
 
 # print all products from a certain shelf
-gondola_id = 5
-shelf_id = 3
+gondola_id = 1
+shelf_id = 5
+# plate_id = 5
 
 product_ids = BookKeeper.getProductIDsFromPosition(gondola_id, shelf_id)
 for product_id in product_ids:
