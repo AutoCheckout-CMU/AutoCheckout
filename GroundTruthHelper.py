9028395f958857e47d9c192b1c5d48a2b7be1ffa
Mengmeng ZHANG <qiuyu8290@gmail.com>
2020-04-18 12:27:56 -0700

    add part of ground truth and add GroundTruth helper

--- GroundTruthHelper.py
+++ GroundTruthHelper.py
@@ -1,13 +1,41 @@
 import BookKeeper
+import json
+
+class Serializable:
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=True, indent=2)
+
+class Product(Serializable):
+    def __init__(self, id, barcodeType, name, thumbnail, price, weight):
+        self.id = id
+        self.barcodeType = barcodeType
+        self.name = name
+        self.thumbnail = thumbnail
+        self.price = price
+        self.weight = weight
 
 # print all products from a certain shelf
-gondola_id = 2
-shelf_id = 4
-# plate_id = 5
+gondola_id = 4
+shelf_id = 6
+plate_id = None
 
-bk = BookKeeper.BookKeeper(dbname='cps-test-12')
+bk = BookKeeper.BookKeeper(dbname='TEAM-PEI-JD-1')
 
-product_ids = bk.getProductIDsFromPosition(gondola_id, shelf_id)
-for product_id in product_ids:
-    product = bk.getProductByID(product_id)
-    print (product)
+if plate_id is not None:
+    product_id = bk.getProductIDsFromPosition(gondola_id, shelf_id, plate_id)
+    productExtended = bk.getProductByID(product_id)
+    objProduct = Product(
+                    productExtended.barcode,
+                    productExtended.barcode_type,
+                    productExtended.name,
+                    productExtended.thumbnail,
+                    productExtended.price,
+                    productExtended.weight
+                )
+    print (objProduct.toJSON())
+else:
+    product_ids = bk.getProductIDsFromPosition(gondola_id, shelf_id)
+    for product_id in product_ids:
+        product = bk.getProductByID(product_id)
+        print (product)
