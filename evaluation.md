1884dc57b3bd3b70c547bd355aaffcad5f64aaad
leochli <leo.chenghui.li@gmail.com>
2020-04-19 23:57:49 -0700

    competition evaluation metrics

--- evaluation.md
+++ evaluation.md
@@ -1,3 +1,24 @@
+April 19th 2020
+New evaluation: Precision, Recall, F1 for competition purpose
+DEBUG = 0 (exact competition mode)
+================== Evaluation Summary ==================
+Databases:  ['cps-test-01', 'cps-test-2', 'cps-test-4', 'cps-test-5', 'cps-test-6', 'cps-test-7', 'cps-test-8', 'cps-test-9', 'cps-test-10', 'cps-test-11', 'cps-test-12', 'cps-test-19', 'ALL-SIMPLE-CHIP-1', 'TEAM-PEI-1', 'TEAM-PEI-JD-1', 'TEAM-8-1']
+Ground truth version:  ground_truth/v12.json
+Overall products detection rate is: 97.6%
+Overall association accuracy is: 97.3%
+Overall precision is: 89.0%
+Overall recall is: 86.9%
+
+DEBUG = 1 (with putback events)
+================== Evaluation Summary ==================
+Databases:  ['cps-test-01', 'cps-test-2', 'cps-test-4', 'cps-test-5', 'cps-test-6', 'cps-test-7', 'cps-test-8', 'cps-test-9', 'cps-test-10', 'cps-test-11', 'cps-test-12', 'cps-test-19', 'ALL-SIMPLE-CHIP-1', 'TEAM-PEI-1', 'TEAM-PEI-JD-1', 'TEAM-8-1']
+Ground truth version:  ground_truth/v12.json
+Overall precision is: 100.0%
+Overall recall is: 86.9%
+Overall F1 is: 93.0%
+
+*****************************************************************************************************************************************************************************************
+
 DEBUG = 1 (include putback event)
 ================== Evaluation Summary ==================
 Databases:  ['cps-test-01', 'cps-test-2', 'cps-test-4', 'cps-test-5', 'cps-test-6', 'cps-test-7', 'cps-test-8', 'cps-test-9', 'cps-test-10', 'cps-test-11', 'cps-test-12', 'cps-test-19', 'ALL-SIMPLE-CHIP-1', 'TEAM-PEI-1', 'TEAM-PEI-JD-1', 'TEAM-8-1']
--- evaluation.py
+++ evaluation.py
@@ -7,7 +7,7 @@ import json
 
 """
 Groundtruth file contains pickup event and putback event separately.
-remove_putback_products() will remove the putback event as well as its coresponding pickup event,
+remove_putback_products() will remove the putback event as well as its corresponding pickup event,
 so that the ground truth only contains file result. 
 """
 def remove_putback_products(gt_list):
@@ -49,32 +49,19 @@ def evaluate_intenvory(dbs, gt_path):
 
     # Metrics 
     tp, fp, tn, fn = 0, 0, 0, 0
-    tp_asso = 0
-    overall_num_preds, overall_products = 0, 0
+    overall_num_preds, overall_num_gt = 0, 0
     for i in range(len(dbs)):
         print("\n\nEvaluating database: ", dbs[i])
+        # Metrics per database
+        db_tp, db_fp, db_tn, db_fn = 0, 0, 0, 0
+        db_pred_counts, db_gt_counts = 0, 0
+
         ########## Generate Prediction ##########
-        predicted_products = {} # Dictionary of all predicted products Key: ProductID Value: Quantity
         dbName = dbs[i]
         myCashier = Cashier()
         receipts = myCashier.process(dbName)
-        num_preds = 0
-        for customerID, customer_receipt in receipts.items():
-            for productID, entry in customer_receipt.purchaseList.items():
-                product, quantity = entry
-                if productID in predicted_products:
-                    predicted_products[productID]['quantity'] += quantity
-                else:
-                    predicted_products[productID] = {'customerID': customerID, 'quantity': quantity}
-                num_preds += quantity
-        if VERBOSE:
-            print("Inventory Change in {}: ".format(dbName), predicted_products, " Amount: ", num_preds)
 
         ########## Evaluate Ground truth ##########
-        num_events = 0
-        num_products = 0
-        num_correct_products = 0
-        num_correct_asso = 0
         gt_entry = gt_list[i]
         # Find groundtruth entry for this database
         tmp_i = i
@@ -85,42 +72,58 @@ def evaluate_intenvory(dbs, gt_path):
         assert (gt_entry['dataset'] == dbName)
         event_list = gt_entry['events']
         for event in event_list:
-            num_events += 1
-            products = event['observation']['products']
+            gt_products = event['observation']['products']
             gt_customerID = event['observation']['target_id']
-            for product in products:
-                num_products += 1
-                gt_productID = product['id']
-                ## Product
-                # Listed in our predicted receipts
-                if (gt_productID in predicted_products and predicted_products[gt_productID]['quantity'] > 0):
-                    # print("GT Identity: ", gt_customerID, "Predicted: ", predicted_products[gt_productID]['customerID'])
-                    if (predicted_products[gt_productID]['customerID'] == gt_customerID):
-                        tp_asso += 1
-                        num_correct_asso += 1
-                    predicted_products[gt_productID]['quantity'] -= 1
-                    if (predicted_products[gt_productID]['quantity'] == 0):
-                        del predicted_products[gt_productID]
-                    tp += 1
-                    num_correct_products += 1
-                else:
-                    fn += 1
-                #     print("Product: ", num_products)
-        for _, entry in predicted_products.items():
-            fp += entry['quantity']
+            # Find the corresponding customer receipts
+            if (gt_customerID in receipts):
+                # Find the corresponding products on the receipt
+                customer_receipt = receipts[gt_customerID]
+                for gt_product in gt_products:
+                    gt_productID = gt_product['id']
+                    for productID, entry in customer_receipt.purchaseList.items():
+                        if (gt_productID == productID):
+                            product, quantity = entry
+                            if (quantity > 0):
+                                db_tp += 1
+                                customer_receipt.purchaseList[productID] = (product, quantity-1) # entry = (product, quantity)
+                    db_gt_counts += 1
+            else:
+                # No such customer
+                for gt_product in gt_products:
+                    db_gt_counts += 1
 
-        overall_num_preds += num_preds
-        overall_products += num_products
-        print("Detected products for {}: {}/{}, correct products {}/{}, correct association {}/{}".format(
-            dbName, num_preds, num_products, num_correct_products, num_preds, num_correct_asso, num_correct_products))
+        # Print False Items
+        if VERBOSE:
+            for id, customer_receipt in receipts.items():
+                print("============== False Receipt {} ==============".format(num_receipt))
+                print("Customer ID: " + id)
+                print("Purchase List: ")
+                for _, entry in customer_receipt.purchaseList.items():
+                    product, quantity = entry
+                    print("*Name: "+product.name + ", Quantities: " + str(quantity), product.thumbnail)
+                    db_fp += quantity
+                num_receipt += 1
+        
+        db_fn = db_gt_counts - db_tp
+        db_pred_counts = db_tp + db_fp
+        overall_num_preds += db_pred_counts
+        overall_num_gt += db_gt_counts
+        tp += db_tp
+        fp += db_fp
+        fn += db_fn
+
+        # Display DB Evaluation
+        print("Database: {}, Correct Items on Receipts: {}/{}, Total GT items: {}".format(dbName, db_tp, db_pred_counts, db_gt_counts))
     
     print("\n================== Evaluation Summary ==================")
     print("Databases: ", dbs)
     print("Ground truth version: ", gt_path)
-    print("Overall products detection rate is: {:.1f}%".format(overall_num_preds*100.0 / overall_products))
-    print("Overall association accuracy is: {:.1f}%".format(tp_asso*100.0/ tp))
-    print("Overall precision is: {:.1f}%".format(tp*100.0 / (tp+fp)))
-    print("Overall recall is: {:.1f}%".format(tp*100.0 / (tp+fn)))
+    precision = tp*100.0 / (tp+fp)
+    print("Overall precision is: {:.1f}%".format(precision))
+    recall = tp*100.0 / (tp+fn)
+    print("Overall recall is: {:.1f}%".format(recall))
+    f1 = 2*precision*recall/(precision+recall)
+    print("Overall F1 is: {:.1f}%".format(f1))
 
 if __name__ == "__main__":
     dbs=['cps-test-01', 'cps-test-2'] + ['cps-test-'+str(i) for i in range(4, 13)]+ ['cps-test-19', 'ALL-SIMPLE-CHIP-1', 'TEAM-PEI-1', 'TEAM-PEI-JD-1', 'TEAM-8-1']
