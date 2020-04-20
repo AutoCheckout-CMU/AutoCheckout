39d8b0a601c489c0eb7e5de2b969c95e5c85f6ef
Mengmeng ZHANG <qiuyu8290@gmail.com>
2020-04-20 13:24:54 -0700

    gt for BASELINE-3

--- evaluation.py
+++ evaluation.py
@@ -122,9 +122,9 @@ def evaluate_intenvory(dbs, gt_path):
     print("\n================== Evaluation Summary ==================")
     print("Databases: ", dbs)
     print("Ground truth version: ", gt_path)
-    precision = tp*100.0 / (tp+fp)
+    precision = 0.0 if tp+fp == 0 else tp*100.0 / (tp+fp)
     print("Overall precision is: {:.1f}%".format(precision))
-    recall = tp*100.0 / (tp+fn)
+    recall = 0.0 if tp+fn == 0 else tp*100.0 / (tp+fn)
     print("Overall recall is: {:.1f}%".format(recall))
     if (precision+recall == 0):
         f1 = 0
@@ -135,6 +135,6 @@ def evaluate_intenvory(dbs, gt_path):
 if __name__ == "__main__":
     # dbs=['cps-test-01', 'cps-test-2'] + ['cps-test-'+str(i) for i in range(4, 13)]+ ['cps-test-19', 'ALL-SIMPLE-CHIP-1', 'TEAM-PEI-1', 'TEAM-PEI-JD-1', 'TEAM-8-1']
     # dbs=['cps-test-19', 'ALL-SIMPLE-CHIP-1', 'TEAM-PEI-1', 'TEAM-PEI-JD-1', 'TEAM-8-1']
-    dbs = ['BASELINE-1', 'BASELINE-2']
+    dbs = ['BASELINE-1','BASELINE-2','BASELINE-3']
     gt_path='ground_truth/v14.json' # list of ground truth W.R.T the previous databases
     evaluate_intenvory(dbs, gt_path)
