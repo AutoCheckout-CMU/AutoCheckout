1a29e3e5d65fc40c049074b77900ab5cbeb7cc03
Yixin Bao <mint.mr@gmail.com>
2020-04-21 14:42:50 -0700

    Fix Time Travel Bug

--- TimeTravel.py
+++ TimeTravel.py
@@ -56,19 +56,19 @@ for testCase in testCases:
                     with zipfile.ZipFile(zipPath,"r") as zip_ref:
                         zip_ref.extractall(videosDir)
                     os.unlink(zipPath)
-                    timestamps = []
-                    for videoName in os.listdir(videosDir + name):
-                        # print(videoName)
-                        split1 = videoName.split('_')
-                        dateStr, fooTimeStr = split1[1], split1[2]
-                        timeStr = fooTimeStr.split('.')[0]
-                        completeTimeStr = dateStr + '_' + timeStr + ' +0200'
-                        # '2020-04-20_07-33-37'
-                        datetimeObj = datetime.strptime(completeTimeStr, '%Y-%m-%d_%H-%M-%S %z')
-                        timestamps.append(datetimeObj.timestamp())
-                    timestamps.sort()
-                    testCaseStartTime[name] = timestamps[0]
-                    print(testCaseStartTime)
+        timestamps = []
+        for videoName in os.listdir(dirForCurrentTestCase):
+            # print(videoName)
+            split1 = videoName.split('_')
+            dateStr, fooTimeStr = split1[1], split1[2]
+            timeStr = fooTimeStr.split('.')[0]
+            completeTimeStr = dateStr + '_' + timeStr + ' +0200'
+            # '2020-04-20_07-33-37'
+            datetimeObj = datetime.strptime(completeTimeStr, '%Y-%m-%d_%H-%M-%S %z')
+            timestamps.append(datetimeObj.timestamp())
+        timestamps.sort()
+        testCaseStartTime[name] = timestamps[0]
+        print(testCaseStartTime)
 
     archivePath = archivesDir + name + '.archive'
     if os.path.exists(archivePath):
